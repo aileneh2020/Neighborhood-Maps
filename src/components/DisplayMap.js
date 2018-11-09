1d18a2b081f8bb6e1629155e038f5d31d293ce3f
Ailene-Hoang <aileneh2020@gmail.com>
2018-11-09 00:02:25 -0800

    Fetch data from FourSquare and added photo to InfoWindow

--- src/components/DisplayMap.js
+++ src/components/DisplayMap.js
@@ -3,27 +3,76 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
 import '../App.css'
 
 const mapKey='AIzaSyBuL7mjc3O3hhrdTWuHRwdKv6k11bUbisk'
+const fsClientID='0KIQPKH3YXQ0JVIUFLOKB315XNQ2VNE31YRMWYIRFT42S3CV'
+const fsClientSecret='OYN5K2A3JMLM3FHP2OVB3CVK4CLP5FJDPXJNSRPHFJVWDRGV'
+const fsVersion='20181108'
 
 class DisplayMap extends Component {
 	state = {
 		map: null,
 		showInfoWindow: false,
 		activeMarker: {},
-		selectedLoc: {}
+		selectedLoc: {},
+		currMarker: {}
 	}
 
 	mapLoaded = (map) => {
 		this.setState({map})
-		this.loadMarkers(this.props.restaurants)
 	}
 
-	// Add props of activeMarker to state and show InfoWindow
-	onMarkerClick = (props, marker, event) => {
-		// Stop animation on any existing marker
+	getVenueData = (props, fsData) => {
+		// Compare FourSquare data for matching restaurant name
+		// Accept names that are longer or shorter versions of same restaurant
+		return fsData.response.venues.filter(item =>
+			item.name.includes(props.name) || props.name.includes(item.name)
+		)
+	}
+
+	onMarkerClick = (props, marker) => {
+		let baseUrl = `https://api.foursquare.com/v2/venues/search?client_id=${fsClientID}&client_secret=${fsClientSecret}&v=${fsVersion}&ll=${props.position.lat},${props.position.lng}&llAcc=100`
+		let headers = new Headers();
+		let request = new Request(baseUrl, {
+			method: 'GET',
+			headers
+		});
+		let currMarker
+
+		// Request data from FourSquare for current marker
+		fetch(request)
+			.then(response => response.json())
+			.then(result => {
+				let currRestaurant = this.getVenueData(props, result)
+				this.setState({
+					currMarker: {
+						...props,
+						fs: currRestaurant[0]
+					}
+				})
+
+				// If FourSquare data found, search for venue's photo
+				if (this.state.currMarker.fs) {
+					let detailUrl = `https://api.foursquare.com/v2/venues/${currRestaurant[0].id}/photos?client_id=${fsClientID}&client_secret=${fsClientSecret}&v=${fsVersion}&limit=1`
+					fetch(detailUrl)
+						.then(response => response.json())
+						.then(result => {
+							console.log(result)
+							this.setState({
+								currMarker: {
+									...currMarker,
+									image: result.response.photos
+								}
+							})
+							console.log(this.state.currMarker)
+						})
+				}
+			})
+
+		// Stop animation on any previous active marker
 		if (this.state.showInfoWindow) {
 			this.state.activeMarker.setAnimation(this.props.google.maps.Animation.null)
 		}
 
+		// Add props to activeMarker state, display InfoWindow, make marker bounce
 		this.setState({
 			selectedLoc: props,
 			activeMarker: marker,
@@ -32,7 +81,7 @@ class DisplayMap extends Component {
 		marker.setAnimation(this.props.google.maps.Animation.BOUNCE)
 	}
 
-	// Reset activeMarker state and close InfoWindow
+	// Reset activeMarker state and close InfoWindow when "X" is clicked
 	onClose = (props) => {
 		if (this.state.showInfoWindow) {
 			this.state.activeMarker.setAnimation(this.props.google.maps.Animation.null)
@@ -43,7 +92,7 @@ class DisplayMap extends Component {
 		}
 	}
 
-	// Close InfoWindow if user clicks anywhere on map
+	// Reset activeMarker state and close InfoWindow when map is clicked
 	onMapClick = (props) => {
 		if (this.state.showInfoWindow) {
 			this.state.activeMarker.setAnimation(this.props.google.maps.Animation.null)
@@ -54,14 +103,11 @@ class DisplayMap extends Component {
 		}
 	}
 
+	// THIS FUNCTION MAY BE IRRELEVANT, CONSIDER DELETE
 	loadMarkers = (restaurants) => {
 		if (!restaurants) {
 			return alert('No restaurants to show')
 		}
-
-		restaurants.restaurants.map((restaurant, index) => {
-			console.log(restaurant)
-		})
 	}
 
 	render() {
@@ -90,6 +136,7 @@ class DisplayMap extends Component {
 						<Marker
 							name={rest.name}
 							address={rest.street}
+							url={rest.url}
 							position={rest.location}
 							key={index}
 							onClick={this.onMarkerClick}
@@ -99,10 +146,20 @@ class DisplayMap extends Component {
 						<InfoWindow
 							marker={this.state.activeMarker}
 							visible={this.state.showInfoWindow}
-							onClose={this.onClose}>
+							onClose={this.onClose}
+						>
 							<div>
 								<h4 className='infoText'>{this.state.selectedLoc.name}</h4>
-								<h4 className='infoText'>{this.state.selectedLoc.address}</h4>
+								{this.state.selectedLoc.url ?
+									<a href={this.state.selectedLoc.url} target='_blank'>{this.state.selectedLoc.url}</a> :
+									''
+								}
+								<div className='infoImage'>
+									{this.state.currMarker.image ?
+									<img src={this.state.currMarker.image.items[0].prefix + 'cap100' + this.state.currMarker.image.items[0].suffix}/> :
+									''
+									}
+								</div>
 							</div>
 						</InfoWindow>
 				</Map>
